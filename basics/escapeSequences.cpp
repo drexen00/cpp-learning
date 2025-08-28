4e8749e628798bba3b7fb0b6e47947f08d379fb5
drexen00 <demensipepro@gmail.com>
2025-08-28 21:33:45 +0500

    Add files via upload

---
+++ basics/escapeSequences.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "we are \"vikings\"." << endl; // double quote ----> \"  some text  \"
+
+    cout << "I have no \'enemies\'. " << endl; // single quote  ----> \' some text again \'
+
+    cout << "calling a backlash ... waiting him and -----> \\" << endl; /* backslash ----> \\ */ 
+
+    cout << "new line boy without \"endl\" \n"; //new line ----> \n
+
+    cout << "some tab))\t\t\t\t you can see that?\n\n\n\n";// tab ----> \t
+
+
+    // poem from A.A. Milne.
+
+    cout << "\t\t\tWhen I was One,\n";
+    cout << "\t\t\tI had just begun.\n\n";
+
+    cout << "\t\t\tWhen I was Two,\n";
+    cout << "\t\t\tI was nearly new.\n\n";
+
+    cout << "\t\t\tWhen I was Three,\n";
+    cout << "\t\t\tI was hardly me.\n\n";
+
+    cout << "\t\t\tWhen I was Four,\n";
+    cout << "\t\t\tI was not much more.\n\n";
+
+    cout << "\t\t\tWhen I was Five,\n";
+    cout << "\t\t\tI was just alive.\n\n";
+
+    cout << "\t\t\tNow I am Six,\n";
+    cout << "\t\t\tI am as clever as clever,\n";
+    cout << "\t\t\tSo I think i\'ll be now for ever and ever.\n";
+
+    
+    return 0;
+}
